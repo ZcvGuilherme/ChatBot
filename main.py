a1ee4cb567bf6e765a2a5764fd4c3b2f01ac30f9
xFrostzss <kaykysilva032006@gmail.com>
2024-08-24 19:38:29 -0300

    m

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ chave_api = 'AIzaSyC9dxA8gHoyVR_OgpyfRaHxdRNjTJUSyDI'
 genai.configure(api_key= chave_api)
 model = genai.GenerativeModel(
     model_name = "gemini-1.5-flash", 
-    system_instruction= 'Você é um professor de estatística que só responde perguntas de estatística.')
+    system_instruction= 'Você é um professor de estatística que só responde perguntas de estatística e não deve responder perguntas fora desse contexto.')
 chat = model.start_chat(
     history=[
         {'role': 'user', 'parts': 'Olá'},
